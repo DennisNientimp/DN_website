544aab3c049f24539898b399e17f6fd2ccb7b5f1
DennisNientimp <your@email.com>
2026-07-08 17:13:00 +0200

    initial website

---
+++ Untitled.R
@@ -0,0 +1,22 @@
+#Making my own website
+
+install.packages("blogdown")
+blogdown::install_hugo()
+
+library(blogdown)
+
+install.packages("blogdown")
+blogdown::install_hugo(force = TRUE)
+
+
+setwd("/Users/dennisnientimp/Desktop/Work/webpage/academic_webpage")
+Sys.setenv(PATH = paste(Sys.getenv("PATH"), "/opt/homebrew/bin", sep = ":"))
+blogdown::build_site()
+
+blogdown::build_site()
+
+blogdown::serve_site()
+
+project:
+  type: website
+
